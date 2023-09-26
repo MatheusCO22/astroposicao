98862775815d41bb831f503b4597b3236e45b24c
MatheusCO22 <matheusco22@hotmail.com>
2023-09-26 10:40:13 -0300

    Correções na tabela

--- src/tablereader.py
+++ src/tablereader.py
@@ -7,7 +7,8 @@ def trim_all_columns(df):
     Trim whitespace from ends of each value across all series in dataframe
     """
     trim_strings = lambda x: x.strip() if isinstance(x, str) else x
-    return df.applymap(trim_strings)
+    #return df.applymap(trim_strings)
+    return df.map(trim_strings)
     
 def get_dataframe():
     with open('./assets/catalog.html') as file:
